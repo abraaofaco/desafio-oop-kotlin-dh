3c856002692a042507ee8e7619d14b9bd494fd28
Abraão Facó <abraaofaco@hotmail.com>
2020-09-28 11:59:47 -0300

    Adiciona classe Aluno e teste na Main

--- src/br/com/digitalhouse/desafio/oop/kotlin/Main.kt
+++ src/br/com/digitalhouse/desafio/oop/kotlin/Main.kt
@@ -1,5 +1,10 @@
 package br.com.digitalhouse.desafio.oop.kotlin
 
 fun main() {
+    val alunoAbraao = Aluno(1, "Abraão", "Facó")
+    val eAlunoAbraao = Aluno(1, "Abraão", "Facó")
 
+    println(alunoAbraao)
+
+    println(alunoAbraao.equals(eAlunoAbraao))
 }
