48c02f3a21cf6ebebe680f1f88975dfade8d0196
Abraão Facó <abraaofaco@hotmail.com>
2020-09-28 17:34:21 -0300

    Adiciona propriedade cursos a classe DigitalHouseManager ref. a Parte G

--- src/br/com/digitalhouse/desafio/oop/kotlin/DigitalHouseManager.kt
+++ src/br/com/digitalhouse/desafio/oop/kotlin/DigitalHouseManager.kt
@@ -7,6 +7,9 @@ class DigitalHouseManager {
     var professores = mutableListOf<Professor>()
     private set
 
+    var cursos = mutableSetOf<Curso>()
+    private set
+
     var matriculas = mutableSetOf<Matricula>()
     private set
 }
