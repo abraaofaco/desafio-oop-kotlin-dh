8e2286dbd7eb1fabf6674f8dd5822be1ab699aeb
Abraão Facó <abraaofaco@hotmail.com>
2020-09-28 14:04:03 -0300

    Ajusta classe Professor para interface e adiciona classes ProfessorTitular, ProfessorAdjunto

--- src/br/com/digitalhouse/desafio/oop/kotlin/Professor.kt
+++ src/br/com/digitalhouse/desafio/oop/kotlin/Professor.kt
@@ -1,26 +1,5 @@
 package br.com.digitalhouse.desafio.oop.kotlin
 
-data class Professor(override val codigo: Int,
-                     override val nome: String,
-                     override val sobrenome: String,
-                     val tempoDeCasa: Int) : Pessoa {
-
-    override fun equals(other: Any?): Boolean {
-        if (this === other) return true
-        if (javaClass != other?.javaClass) return false
-
-        other as Professor
-
-        if (codigo != other.codigo) return false
-
-        return true
-    }
-
-    override fun hashCode(): Int {
-        return codigo
-    }
-
-    override fun toString(): String {
-        return "Professor(código=$codigo, nome=$nomeCompleto, tempo de casa=$tempoDeCasa)"
-    }
+interface Professor : Pessoa {
+    val tempoDeCasa: Int
 }
---
+++ src/br/com/digitalhouse/desafio/oop/kotlin/ProfessorTitular.kt
@@ -0,0 +1,27 @@
+package br.com.digitalhouse.desafio.oop.kotlin
+
+data class ProfessorTitular(override val codigo: Int,
+                            override val nome: String,
+                            override val sobrenome: String,
+                            override val tempoDeCasa: Int,
+                            val especialidade: String) : Professor {
+
+    override fun equals(other: Any?): Boolean {
+        if (this === other) return true
+        if (javaClass != other?.javaClass) return false
+
+        other as ProfessorTitular
+
+        if (codigo != other.codigo) return false
+
+        return true
+    }
+
+    override fun hashCode(): Int {
+        return codigo
+    }
+
+    override fun toString(): String {
+        return "Professor Titular(código=$codigo, nome=$nomeCompleto, tempo de casa=$tempoDeCasa, especialidade='$especialidade')"
+    }
+}
